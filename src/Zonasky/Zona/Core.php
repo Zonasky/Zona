667128f153da09eee4cba0970723411879fe99cf
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-06-19 17:35:38 +1000

    Update Core.php

--- src/Zonasky/Zona/Core.php
+++ src/Zonasky/Zona/Core.php
@@ -7,14 +7,6 @@ use pocketmine\Player;
 //TODO: 1.0.5: add reload commands and whatnot
 // CODE REWRITE INCOMING
 Class Core extends PluginBase {
-const errortype = 
-  [
-1 = "Syntax Error",
-  2 = "Argument should return bool, bool not found",
-  3 = "sky was here, you just suck at coding if you get this",
-
-  ];
-return $errortype;
 
   private function getscripts(string $scripts) : bool {
 $this->getResourceFolder("scripts")->getChildren();
@@ -23,13 +15,8 @@ $this->getResourceFolder("scripts")->getChildren();
   }
 
 
-  private function interpret(string $str, Array $args) : bool {
-$errorScriptUnreadble = "ERROR: Your Script cannot be interpreted as it has a " . $errortype . " !";
-  $plr = $this->getPlayer();
-  $script = getscripts();
-    $player = $this->str_replace("%player%", $plr, $script)
-$broadcast = $this->str_replace("send new message to the server", $this->getServer()->broadcastMessage() , $script);
-
+  private function replace(string $string) : bool {
+  return $this->str_replace()
   
 }
 
